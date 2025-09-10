11b271bfc924d66cf0ab5794d3d2ad85d050bdb4
Amandev25 <amanyadav.dev25@gmail.com>
2025-09-10 17:52:15 +0530

    React Project : 3 , Meals

--- Beginners_React_Project/src/App.jsx
+++ Beginners_React_Project/src/App.jsx
@@ -1,11 +1,12 @@
-import Counter from "./Components/Counter/counter"
+import Meals from "./Components/Meals/Meals.jsx"
 import Todo from "./Components/Todos/Todo.jsx"
 function App() {
 
   return (
     <>
     {/* <Counter/> */}
-    <Todo/>
+    
+    <Meals/>
     </>
   )
 }
